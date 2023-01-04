80b36562899114299451bac918db49af6b1e57d7
Maksymilian Żmijewski <makszmijewski1@gmail.com>
2023-01-04 17:06:43 +0100

    pierwszy commit

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,6 +3,17 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
+        Room room1 = new Room(30, 22, true, 18);
+        Room room2 = new Room(25, 24, false);
+        Room room3 = new Room(27, 18.5, true, 18);
 
+        room1.lowerTemperature();
+        room1.lowerTemperature();
+        room2.lowerTemperature();
+        room3.lowerTemperature();
+
+        System.out.println(room1);
+        System.out.println(room2);
+        System.out.println(room3);
     }
 }
